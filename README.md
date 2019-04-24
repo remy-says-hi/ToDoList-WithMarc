f29841a3913c4679e39adcbda11a9ed77b5dc5d6
Jared Farkas and Marc Davies <marc@>
2019-04-24 09:16:01 -0700

    add display

---
+++ README.md
@@ -0,0 +1,42 @@
+# _{Application Name}_
+
+#### _{Brief description of application}, {Date of current version}_
+
+#### By _**Marc Davies**_
+
+## Description
+
+_{This is a detailed description of your application. Its purpose and usage.  Give as much detail as needed to explain what the application does, and any other information you want users or other developers to have. }_
+
+## Setup/Installation Requirements
+
+* _Clone from GitHub_
+* _In your terminal, $mono ProjectName.exe_
+
+## Specs
+
+| Behavior | Input | Output |
+| ------------- |:-------------:| -----:|
+| Lorem | x | true/false |
+| Lorem | x | true/false |
+| Lorem | x | true/false |
+| Lorem | x | true/false |
+| Lorem | x | true/false |
+
+## Known Bugs
+
+_None as of last update_
+
+## Support and contact details
+
+_Please contact me at marcdaviesriot@gmail.com if you run into any issues or have questions, ideas or feedback._
+
+## Technologies Used
+
+_C#_
+
+### License
+
+*This software is licensed under the GPL license.*
+
+Copyright (c) 2019 **_Marc Davies_**
---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,89 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTest : IDisposable
+  {
+
+    public void Dispose()
+    {
+      //Act
+      Item.ClearAll();
+    }
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      //Arrange
+      Item newItem = new Item("test");
+
+      //Assert
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string result = newItem.GetDescription();
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.SetDescription(updatedDescription);
+      string result = newItem.GetDescription();
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      // Arrange
+      List<Item> newList = new List<Item> { };
+
+      // Act
+      List<Item> result = Item.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+  }
+}
